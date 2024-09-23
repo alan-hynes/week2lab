b53c97c6438493e063e1629307ddbe1eab7da22a
G00400498@atu.ie <g00400498@atu.ie>
2024-09-23 16:39:26 +0100

    new project. created and populated product class

---
+++ src/main/java/ie/atu/week2_refresherlab/Week2RefresherlabApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week2_refresherlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2RefresherlabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week2RefresherlabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week2_refresherlab/Week2RefresherlabApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week2_refresherlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2RefresherlabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
