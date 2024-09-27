cc9c0291f8e7581d91db942a3e75f23c0c3b456c
alan-hynes <G00400498@atu.ie>
2024-09-27 18:33:24 +0100

    added validation for product entity

--- src/main/java/ie/atu/week2_refresherlab/Product.java
+++ src/main/java/ie/atu/week2_refresherlab/Product.java
@@ -1,14 +1,19 @@
 package ie.atu.week2_refresherlab;
 
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
+    @NonNull
     private Long id;
+    @NonNull
     private String name;
+    @Positive(message = "Price must be more than 0")
     private double price;
 }
