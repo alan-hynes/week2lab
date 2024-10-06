d97f550f87c82817c2ecd2407503784039b2fa9a
alan-hynes <G00400498@atu.ie>
2024-10-06 18:22:47 +0100

    edited product entity

--- src/main/java/ie/atu/week2_refresherlab/Product.java
+++ src/main/java/ie/atu/week2_refresherlab/Product.java
@@ -1,5 +1,6 @@
 package ie.atu.week2_refresherlab;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
@@ -10,10 +11,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
-    @NotNull
+
+    @Positive(message = "ID must be greater than 0")
     private Long id;
-    @NotNull
+
+    @NotBlank(message = "Product name cannot be blank")
     private String name;
-    @Positive(message = "Price must be more than 0")
+
+    @Positive(message = "Price must be greater than 0")
     private double price;
+
+    @Positive(message = "Stock must be a positive number")
+    private int stock;
 }
